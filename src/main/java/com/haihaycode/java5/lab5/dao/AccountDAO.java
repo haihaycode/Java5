f4f9e905153048025c09aa8faee832a48be52efa
haihaycode <kienthuc177@gmail.com>
2024-05-24 18:02:17 +0700

    lab5 final nha ae

--- src/main/java/com/haihaycode/java5/lab5/dao/AccountDAO.java
+++ src/main/java/com/haihaycode/java5/lab5/dao/AccountDAO.java
@@ -3,4 +3,6 @@ package com.haihaycode.java5.lab5.dao;
 import com.haihaycode.java5.lab5.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountDAO extends JpaRepository<Account, String> {}
+public interface AccountDAO extends JpaRepository<Account, String> {
+
+}
